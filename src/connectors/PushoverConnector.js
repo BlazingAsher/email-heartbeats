0bef4ea5c17a3a90157e589b652a93d0861c40e8
David Hui <opensource@davidhui.ca>
2024-06-06 20:25:16 -0400

    add pushover connection

--- src/connectors/PushoverConnector.js
+++ src/connectors/PushoverConnector.js
@@ -1,4 +1,22 @@
-export async function sendPushoverMessage(app_token, user_token, message, title) {
-    // Send the pushover message here
-    console.log(`Sending pushover message to ${user_token}: ${message}`);
+import axios from "axios";
+
+const pushoverApi = axios.create({
+    "baseURL": "https://api.pushover.net/1/"
+});
+
+export async function sendPushoverMessage (app_token, user_token, message, title) {
+    const requestData = {
+        "token": app_token,
+        "user": user_token,
+        message
+    };
+
+    if (title) {
+        requestData.title = title;
+    }
+
+    return pushoverApi.post(
+        "messages.json",
+        requestData
+    );
 }
